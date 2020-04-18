753ab4882d66f6eec2c2e3c627696a0764741b3f
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-04-18 18:26:47 +0200

    added class Amenities

--- src/beans/Amenities.java
+++ src/beans/Amenities.java
@@ -1,5 +1,31 @@
 package beans;
 
+import java.util.UUID;
+
 public class Amenities {
+	
+	UUID idOne = UUID.randomUUID();
+	private String name;
+	
+	public Amenities(UUID idOne, String name) {
+		super();
+		this.idOne = idOne;
+		this.name = name;
+	}
+	public UUID getIdOne() {
+		return idOne;
+	}
+	public void setIdOne(UUID idOne) {
+		this.idOne = idOne;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
+

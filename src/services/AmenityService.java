4cb9fe927c56441891892b160f59957384e17fae
Mina Maras <minamaras1997@gmail.com>
2020-04-20 11:27:05 +0200

    implemented delete amenity - needs to be fixed

--- src/services/AmenityService.java
+++ src/services/AmenityService.java
@@ -2,10 +2,13 @@ package services;
 
 import javax.ws.rs.core.MediaType;
 
+import java.util.Collection;
+
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,6 +16,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
 import beans.Amenities;
+import beans.AmenityStatus;
 import beans.User;
 import dao.AmenitiesDAO;
 import dao.UserDAO;
@@ -44,6 +48,8 @@ public class AmenityService {
 		if(amenities.getAmenities().containsKey(a.getName())) {
 			return Response.status(400).build();
 		}
+		
+		a.setAmenityStatus(AmenityStatus.ACTIVE);
 
 		amenities.getAmenities().put(a.getName(), a);
 		System.out.println(a.getIdOne().toString());
@@ -70,7 +76,34 @@ public class AmenityService {
 		amenities.saveAmenities(context.getRealPath(""), amenities);
 		
 		return Response.ok(a).build();
+	}
+	
+	@POST
+	@Path("/amenity/delete")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response deleteAmenity(Amenities a, @Context HttpServletRequest request) {
 		
+		AmenitiesDAO amenities = (AmenitiesDAO) context.getAttribute("AmenitiesDAO");
+		
+		a.setAmenityStatus(AmenityStatus.DELETED);
+			
+		context.setAttribute("AmenitiesDAO", amenities);
+		
+		amenities.saveAmenities(context.getRealPath(""), amenities);
+		
+		return Response.ok().build();
+	}
+	
+	@GET
+	@Path("/amenity/all")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<Amenities> all(@Context HttpServletRequest request) {
+		
+		AmenitiesDAO amenities = (AmenitiesDAO) context.getAttribute("AmenitiesDAO");		
+		
+		return amenities.getAllAmenities();
 	}
 	
 

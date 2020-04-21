78e78f1b60a547a7970a6a21d975bf50ac625ab9
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-04-21 19:11:05 +0200

    added class Location

--- src/beans/Location.java
+++ src/beans/Location.java
@@ -1,5 +1,50 @@
 package beans;
 
 public class Location {
+	
+	private float gWidth;
+	private float gHeight;
+	private Address adress;
+	
+	public Location() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Location(float gWidth, float gHeight, Address adress) {
+		super();
+		this.gWidth = gWidth;
+		this.gHeight = gHeight;
+		this.adress = adress;
+	}
+
+	public float getgWidth() {
+		return gWidth;
+	}
+
+	public void setgWidth(float gWidth) {
+		this.gWidth = gWidth;
+	}
+
+	public float getgHeight() {
+		return gHeight;
+	}
+
+	public void setgHeight(float gHeight) {
+		this.gHeight = gHeight;
+	}
+
+	public Address getAdress() {
+		return adress;
+	}
+
+	public void setAdress(Address adress) {
+		this.adress = adress;
+	}
+
+	
+	
+	
+	
 
 }

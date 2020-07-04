d9027716014a524e239d430ddf860ccdce6ec8dd
Mina Maras <minamaras1997@gmail.com>
2020-07-04 15:15:40 +0200

    Create Filter.java

---
+++ src/beans/Filter.java
@@ -0,0 +1,94 @@
+package beans;
+
+public class Filter {
+
+	private String numberOfRoomsFrom;
+	private String numberOfRoomsTo;
+	private String numberOfGuests;
+	private String location;
+	private String startDate;
+	private String endDate;
+	private String startPrice;
+	private String endPrice;
+	
+	public Filter() {
+		
+	}
+	
+	
+
+	public String getNumberOfRoomsFrom() {
+		return numberOfRoomsFrom;
+	}
+
+
+
+	public void setNumberOfRoomsFrom(String numberOfRoomsFrom) {
+		this.numberOfRoomsFrom = numberOfRoomsFrom;
+	}
+
+
+
+	public String getNumberOfRoomsTo() {
+		return numberOfRoomsTo;
+	}
+
+
+
+	public void setNumberOfRoomsTo(String numberOfRoomsTo) {
+		this.numberOfRoomsTo = numberOfRoomsTo;
+	}
+
+
+
+	public String getNumberOfGuests() {
+		return numberOfGuests;
+	}
+
+	public void setNumberOfGuests(String numberOfGuests) {
+		this.numberOfGuests = numberOfGuests;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getStartPrice() {
+		return startPrice;
+	}
+
+	public void setStartPrice(String startPrice) {
+		this.startPrice = startPrice;
+	}
+
+	public String getEndPrice() {
+		return endPrice;
+	}
+
+	public void setEndPrice(String endPrice) {
+		this.endPrice = endPrice;
+	}
+	
+	
+	
+}

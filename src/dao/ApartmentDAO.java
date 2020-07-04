aff952e9f0ddf6db19dcc9df1c4887ce7b821e0b
Mina Maras <minamaras1997@gmail.com>
2020-07-04 15:15:48 +0200

    filter apartments

--- src/dao/ApartmentDAO.java
+++ src/dao/ApartmentDAO.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,6 +24,7 @@ import beans.Amenities;
 import beans.Apartment;
 import beans.ApartmentStatus;
 import beans.ApartmentType;
+import beans.Filter;
 import beans.RentPeriod;
 import beans.User;
 
@@ -241,6 +243,88 @@ public class ApartmentDAO {
 
 	public void setApartments(HashMap<String, Apartment> apartments) {
 		this.apartments = apartments;
+	}
+	
+	public ArrayList<Apartment> filterApartmentsByLocation(String location, ArrayList<Apartment> apps) {
+		for(Apartment a : apps) {
+			if(!a.getLocation().getAddress().getCity().equals(location)) {
+				apps.remove(a);
+			}
+		}
+		return apps;
+	}
+	
+	public ArrayList<Apartment> filterApartmentsByGuests(String guests, ArrayList<Apartment> apps) {
+		for(Apartment a : apps) {
+			if(!(a.getNumberOfGuests() == Integer.parseInt(guests))) {
+				apps.remove(a);
+			}
+		}
+		return apps;
+	}
+	
+	public ArrayList<Apartment> filterApartmentsByPrice(String startPrice, String endPrice, ArrayList<Apartment> apps) {
+		for(Apartment a : apps) {
+			if(!(a.getPrice() >= Float.parseFloat(startPrice) && a.getPrice() <= Float.parseFloat(endPrice))) {
+				apps.remove(a);
+			}
+		}
+		return apps;
+	}
+	
+	public ArrayList<Apartment> filterApartmentsByDate(String startDate, String endDate, ArrayList<Apartment> apps) {
+		LocalDate start = LocalDate.parse(startDate);
+		LocalDate end = LocalDate.parse(endDate);
+		
+		List<String> totalDates = new ArrayList<>();
+
+	    while (!start.isAfter(end)) {
+	      totalDates.add(start.toString());
+	      start = start.plusDays(1);
+	    }
+
+		for(Apartment a : apps) {
+			ArrayList<String> dates = a.getFreeDates();
+
+			if(!(dates.contains(start) && dates.contains(end))) {
+				apps.remove(a);
+				continue;
+			}
+			
+			for(String d : totalDates) {
+				if(!dates.contains(d)) {
+					apps.remove(a);
+				}
+			}
+		}
+		return null;
+	}
+	
+	public ArrayList<Apartment> filterApartmentsByRooms(String from, String to, ArrayList<Apartment> apps) {
+		for(Apartment a : apps) {
+			if(!(a.getNumberOfGuests() >= Float.parseFloat(from) && a.getNumberOfGuests() <= Float.parseFloat(to))) {
+				apps.remove(a);
+			}
+		}
+		return apps;
+	}
+
+	public ArrayList<Apartment> filterApartments(Filter filter, ArrayList<Apartment> apps) {
+		ArrayList<Apartment> toReturn = apps;
+		
+		if(!filter.getLocation().isEmpty()) {
+			toReturn = filterApartmentsByLocation(filter.getLocation(), toReturn);
+		} else if(!filter.getNumberOfGuests().isEmpty()) {
+			toReturn = filterApartmentsByGuests(filter.getNumberOfGuests(), toReturn);
+		} else if(!filter.getStartPrice().isEmpty() && !filter.getEndPrice().isEmpty()) {
+			toReturn = filterApartmentsByPrice(filter.getStartPrice(), filter.getEndPrice(), toReturn);
+		} else if(!filter.getStartDate().isEmpty() && !filter.getEndDate().isEmpty()) {
+			toReturn = filterApartmentsByDate(filter.getStartDate(), filter.getEndDate(), toReturn);
+		} else if(!filter.getNumberOfRoomsFrom().isEmpty() && !filter.getNumberOfRoomsTo().isEmpty()) {
+			toReturn = filterApartmentsByRooms(filter.getNumberOfRoomsFrom(), filter.getNumberOfRoomsTo(), toReturn);
+		}
+		
+		return toReturn;
 	}
 	
 	
